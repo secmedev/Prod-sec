4a7ad33336d16c7e62b955a01a7e29b23c506c1c
secmedev <121214790+secmedev@users.noreply.github.com>
2026-08-15 15:36:18 +0530

    Update semgrep-test.py

--- semgrep-test.py
+++ semgrep-test.py
@@ -1,10 +1,5 @@
-# semgrep-test.py
-
 import subprocess
 
-def run_command(user_input):
-    # Deliberately vulnerable: user-controlled input reaches a shell.
-    subprocess.run(user_input, shell=True)
-
 user_input = input("Enter command: ")
-run_command(user_input)
+
+subprocess.call(user_input, shell=True)
